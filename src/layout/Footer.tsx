0dff903677db74b06efd516a5083ae79472e181c
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2023-12-28 18:16:05 +0100

    finishing design footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -10,13 +10,25 @@ const Footer = () => (
       </div>
       <div className="col">
         <p>
-          <a href="https://www.natural-solutions.eu/" target="_blank">
+          <a
+            style={{ color: "black" }}
+            href="https://www.natural-solutions.eu/"
+            target="_blank"
+          >
             Natural Solutions
           </a>{" "}
           <br />
           English :{" "}
-          <a href="https://www.natural-solutions.world/" target="_blank">
+          <a
+            style={{ color: "black" }}
+            href="https://www.natural-solutions.world/"
+            target="_blank"
+          >
             natural-solutions.world
+          </a>
+          <br />
+          <a href="https://geocam.natural-solutions.eu/contact/">
+            Nous contacter
           </a>
         </p>
       </div>
@@ -25,16 +37,23 @@ const Footer = () => (
           <li>Comment installer l&apos;application ?</li>
           <li>Documentation utilisateur</li>
           &rarr; Toutes les informations sont sur{" "}
-          <a href="https://github.com/naturalsolutions/geocam">GitHub</a>
+          <a href="https://github.com/naturalsolutions/geocam" target="_blank">
+            GitHub
+          </a>
         </ul>
       </div>
     </div>
     <div className="container text-center">
       <p>
         Site réalisé (2023) par{" "}
-        <a href="https://github.com/MathildeNS">Mathilde Leclerc</a> et{" "}
-        <a href="https://georgemoon.com/">George Moon</a>. Code source
-        disponible{" "}
+        <a style={{ color: "black" }} href="https://github.com/MathildeNS">
+          Mathilde Leclerc
+        </a>{" "}
+        et{" "}
+        <a style={{ color: "black" }} href="https://georgemoon.com/">
+          George Moon
+        </a>
+        . Code source disponible{" "}
         <a href="https://github.com/naturalsolutions/geocam-site">ici</a>
       </p>
     </div>
