ac0ed6ee269637d1c79b0113487639ff8cb71d24
Mathilde Leclerc <mathilde_leclerc@natural-solutions.eu>
2023-12-28 17:58:22 +0100

    feat: complete footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,44 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./static/logo.png";
 import Image from "next/image";
-import Modal from "@/components/presentation/Modal";
 
 const Footer = () => (
-  <div className="constrainer">
-    <footer className="bg-light py-5">
-      <div className="container navbar-text">
+  <footer className="bg-light ">
+    <div className="row container  items-center align-items-center">
+      <div className="col">
         <Image src={logo} height={80} className="me-1" alt="" />
+      </div>
+      <div className="col">
         <p>
           <a href="https://www.natural-solutions.eu/" target="_blank">
             Natural Solutions
           </a>{" "}
-          2023 <br />
+          <br />
           English :{" "}
           <a href="https://www.natural-solutions.world/" target="_blank">
             natural-solutions.world
           </a>
         </p>
-        {/* <a
-          role="button"
-          className={`${<Modal title="test" children="test" />} ${isOpen && "is-active"}`}
-          aria-label="modal"
-          aria-expanded="false"
-          onClick={()=><Modal title="test" children="test" />}
-        >
-          Launch demo modal
-        </a>
-          <Modal title="test" children="test" /> */}
-        <button
-          type="button"
-          className="btn btn-primary"
-          data-bs-toggle="modal"
-          data-bs-target={<Modal />}
-        >
-          Launch demo modal
-        </button>
       </div>
-    </footer>
-  </div>
+      <div className="col">
+        <ul>
+          <li>Comment installer l&apos;application ?</li>
+          <li>Documentation utilisateur</li>
+          &rarr; Toutes les informations sont sur{" "}
+          <a href="https://github.com/naturalsolutions/geocam">GitHub</a>
+        </ul>
+      </div>
+    </div>
+    <div className="container text-center">
+      <p>
+        Site réalisé (2023) par{" "}
+        <a href="https://github.com/MathildeNS">Mathilde Leclerc</a> et{" "}
+        <a href="https://georgemoon.com/">George Moon</a>. Code source
+        disponible{" "}
+        <a href="https://github.com/naturalsolutions/geocam-site">ici</a>
+      </p>
+    </div>
+  </footer>
 );
 
 export default Footer;
